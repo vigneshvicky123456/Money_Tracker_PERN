9147fc427b3360e0d77f95efbaa7b50df1bcb2f0
vigneshvicky123456 <vignesh.j2002@gmail.com>
2025-01-31 12:01:56 +0530

    corrected the database

--- Backend/database.js
+++ Backend/database.js
@@ -1,17 +1,24 @@
+require('dotenv').config(); 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('money_tracker', 'postgres', 'postgres', {
-    host: 'localhost', 
-    dialect: 'postgres'
-});
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,      
+    process.env.DB_USER,      
+    process.env.DB_PASSWORD,  
+    {
+        host: process.env.DB_HOST,  
+        dialect: process.env.DB_DIALECT,  
+    }
+);
 
 // Test connection to the database
-
-sequelize.authenticate().then(() => {
-    console.log('Database connected!');
-  }).catch((error) => {
-    console.error('Unable to connect to the database:', error);
-  });
+sequelize.authenticate()
+    .then(() => {
+        console.log('Database connected!');
+    })
+    .catch((error) => {
+        console.error('Unable to connect to the database:', error);
+    });
 
 module.exports = sequelize;
-
