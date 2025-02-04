86f80ca47763c9ac90a3bd16464322577c926d79
vigneshvicky123456 <vignesh.j2002@gmail.com>
2025-02-04 10:55:43 +0530

    Added render for deploy

--- Backend/database.js
+++ Backend/database.js
@@ -2,15 +2,25 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,      
-    process.env.DB_USER,      
-    process.env.DB_PASSWORD,  
-    {
-        host: process.env.DB_HOST,  
-        dialect: process.env.DB_DIALECT,  
+// const sequelize = new Sequelize(
+//     process.env.DB_NAME,      
+//     process.env.DB_USER,      
+//     process.env.DB_PASSWORD,  
+//     {
+//         host: process.env.DB_HOST,  
+//         dialect: process.env.DB_DIALECT,  
+//     }
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true, 
+        rejectUnauthorized: false
+      }
     }
-);
+  });
 
 // Test connection to the database
 sequelize.authenticate()
